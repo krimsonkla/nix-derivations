3b904b7ffa17bc7c10015cd682f72a557b56523d
Jason Risch <jasonrisch@gmail.com>
2026-09-07 15:31:22 -0700

    test(guards): rev drift probe, rev changed with hash kept

--- pkgs/hello-pinned/default.nix
+++ pkgs/hello-pinned/default.nix
@@ -9,7 +9,7 @@ stdenvNoCC.mkDerivation {
   src = fetchFromGitHub {
     owner = "octocat";
     repo = "Hello-World";
-    rev = "7fd1a60b01f91b314f59955a4e4d4e80d8edf11d";
+    rev = "553c2077f0edc3d5dc5d17262f6aa498e69d6f8e";
     hash = "sha256-gdkPz7VJ8ZOwJ5oetnuXBXPkkHlOsU7w0PghYjWgpAo=";
   };
   dontBuild = true;
